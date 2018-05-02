ce9a4bc5aa92ae63aecdc90f9a554eae3b74ea48
Majudhu <majudhu.sup@gmail.com>
2018-05-02 12:13:37 +0800

    Chapter 11: Facelift

--- app/__init__.py
+++ app/__init__.py
@@ -6,12 +6,14 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from logging.handlers import RotatingFileHandler
 import os
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
+bootstrap = Bootstrap(app)
 from app import routes, models, errors
 
 login.login_view = 'login'
